246294f424b206085a4d748adf06009c64d67c79
first commit <>
2018-05-05 18:16:54 +0900

    AddAboutToUsers column

--- db/schema.rb
+++ db/schema.rb
@@ -10,30 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180504080508) do
+ActiveRecord::Schema.define(version: 20180505091507) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                                default: "", null: false
+    t.string   "encrypted_password",                   default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                        default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "first_name",                          null: false
-    t.string   "last_name",                           null: false
-    t.integer  "birthday_month",                      null: false
-    t.integer  "birthday_day",                        null: false
-    t.integer  "birthday_year",                       null: false
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
+    t.string   "first_name",                                        null: false
+    t.string   "last_name",                                         null: false
+    t.integer  "birthday_month",                                    null: false
+    t.integer  "birthday_day",                                      null: false
+    t.integer  "birthday_year",                                     null: false
     t.string   "gender"
     t.string   "native_currency"
     t.string   "preferred_locale"
     t.integer  "tel_number"
+    t.text     "about",                  limit: 65535
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end

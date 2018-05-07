7412da93abc43e66fdceda9fa8f62d593c2e8c1b
first commit <>
2018-05-07 11:34:11 +0900

    avatar upload setting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    if current_user.update(user_params)
+    if current_user.update(profile_params)
       redirect_to root_path
     else
       render :edit
@@ -11,12 +11,15 @@ class UsersController < ApplicationController
   end
 
   def avatar
-
+    current_user.update(avatar: params[:avatar])
   end
 
   private
 
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :gender, :birthday_month, :birthday_day, :birthday_year, :email, :tel_number, :preferred_locale, :native_currency, :current_city, :about)
+  def profile_params
+    params.require(:user).permit(:first_name, :last_name, :gender, :birthday_month, :birthday_day, :birthday_year, :email, :tel_number, :preferred_locale, :native_currency, :current_city, :about, :avatar)
+  end
+
+  def avatar_params
   end
 end

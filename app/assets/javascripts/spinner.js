67b3450786fa3cf87621768be9df4b22a9abc2f1
first commit <>
2018-05-11 18:52:33 +0900

    counter js create

--- app/assets/javascripts/spinner.js
+++ app/assets/javascripts/spinner.js
@@ -3,32 +3,49 @@ $(function(){
   var step = 1;
   var min = 1;
   var max = 50;
-  var count = $('.bed-count__input').val();
-  var count = parseInt(count);
+  var bedCount = $('.bed-count__input').val();
+  var bedCount = parseInt(bedCount);
+  var capacityCount = $('.capacity-count__input').val();
+  var capacityCount = parseInt(capacityCount);
+  var bathroomCount = $('.bathroom-count__input').val();
+  var bathroomCount = parseInt(bathroomCount);
 
   $('.bed-count__spinner-up').on('click', function(){
-    if ( count < max ){
-      var plus_count = count += step ;
+    if ( bedCount < max ){
+      var plus_count = bedCount += step ;
       $('.bed-count__input').attr('value', plus_count);
     }
   });
   $('.bed-count__spinner-down').on('click', function(){
-    if ( count > min ){
-      var minus_count = count -= step ;
+    if ( bedCount > min ){
+      var minus_count = bedCount -= step ;
       $('.bed-count__input').attr('value', minus_count);
     }
   });
 
   $('.capacity-count__spinner-up').on('click', function(){
-    if ( count < max ){
-      var plus_count = count += step ;
+    if ( capacityCount < max ){
+      var plus_count = capacityCount += step ;
       $('.capacity-count__input').attr('value', plus_count);
     }
   });
   $('.capacity-count__spinner-down').on('click', function(){
-    if ( count > min ){
-      var minus_count = count -= step ;
+    if ( capacityCount > min ){
+      var minus_count = capacityCount -= step ;
       $('.capacity-count__input').attr('value', minus_count);
+    }
+  });
+
+  $('.bathroom-count__spinner-up').on('click', function(){
+    if ( bathroomCount < max ){
+      var plus_count = bathroomCount += step ;
+      $('.bathroom-count__input').attr('value', plus_count);
+    }
+  });
+  $('.bathroom-count__spinner-down').on('click', function(){
+    if ( bathroomCount > min ){
+      var minus_count = bathroomCount -= step ;
+      $('.bathroom-count__input').attr('value', minus_count);
     }
   });
 });

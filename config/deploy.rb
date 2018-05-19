c55f36f40671a6a95b24a49f4290c445a19be74f
first commit <>
2018-05-19 12:02:56 +0900

    deploy.rb fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.10.2"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
-
+set :application, "c-airbnb"
+set :repo_url,  'git@github.com:laster18/c-airbnb.git'
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 

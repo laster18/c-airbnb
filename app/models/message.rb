1be70fe29865f10b99fa11ccefacb88e5cfe8950
first commit <>
2018-05-25 11:36:06 +0900

    association set

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,6 @@
 class Message < ApplicationRecord
+  belongs_to :send_user, class_name: 'User', :foreign_key => 'send_user_id'
+  belongs_to :receive_user, class_name: 'User', :foreign_key => 'receive_user_id'
+
+  enum kind: { star: 0, unread: 1, already_read: 2, appointment: 3 }
 end

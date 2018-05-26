45879abdef1dbb7e1fb878635b66ca75904458ea
first commit <>
2018-05-26 14:17:48 +0900

    messages table fix

--- db/migrate/20180525015123_create_messages.rb
+++ db/migrate/20180525015123_create_messages.rb
@@ -1,11 +1,10 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-      t.string      :title
-      t.text        :messages, null: false
+      t.text        :message, null: false
       t.integer     :send_user_id, null: false
       t.integer     :receive_user_id, null: false
-      t.integer     :kind
+      t.integer     :kind, null: false, default: 0
       t.timestamps
     end
   end

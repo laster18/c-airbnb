36a13a06885e698c9b59a569ec0d307811ea9b2c
first commit <>
2018-05-26 15:37:41 +0900

    send_message view fix

--- app/controllers/send_messages_controller.rb
+++ app/controllers/send_messages_controller.rb
@@ -3,25 +3,24 @@ class SendMessagesController < ApplicationController
   def new
     @room = Room.find(params[:room_id])
     @receive_user = @room.user
-    @send_message = Message.new
+    @message = Message.new
   end
 
   def create
-    binding.pry
     @message = Message.new(message_params)
     if @message.save
       redirect_to root_path
     else
+      @receive_user = User.find(params[:user_id])
       render :new
     end
   end
 
   def destroy
-
   end
 
   private
   def message_params
-    params.require(:send_message).permit(:title, :message).merge(send_user_id: current_user.id)
+    params.require(:message).permit( :message).merge(send_user_id: current_user.id, receive_user_id: params[:user_id])
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,5 @@ class Message < ApplicationRecord
   belongs_to :receive_user, class_name: 'User', :foreign_key => 'receive_user_id'
 
   enum kind: { unread: 0, already_read: 1, star: 2, appointment: 3 }
+  validates :message, presence: true
 end

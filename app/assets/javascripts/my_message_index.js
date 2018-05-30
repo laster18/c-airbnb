d45177735cf9fb991ddf78953884288892ac3f19
first commit <>
2018-05-30 18:28:05 +0900

    message js fix

--- app/assets/javascripts/my_message_index.js
+++ app/assets/javascripts/my_message_index.js
@@ -1,7 +1,6 @@
 $(function(){
-  console.log('test');
-  $('.d-message-top-wrapper__select').on('change',function(){
-    var kind = $('.d-message-top-wrapper__select option:selected').val();
+  $('.message-select-js').on('change',function(){
+    var kind = $('.message-select-js option:selected').val();
     if ( kind == 'all' ) {
       $('.d-message-item').css('display', 'block')
     } else {
